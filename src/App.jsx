a8f539e1f118566dc2a7375b49e749f8a13faf0d
NIkhil Sahu <117375980+nikhilsahu2002@users.noreply.github.com>
2024-03-24 00:13:04 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ function App() {
     <>
       <Routes>
         <Route
+          exact
           path="/"
           element={
             <>
@@ -30,6 +31,7 @@ function App() {
         />
         <Route
           path="/intership"
+          exact
           element={
             <>
               <Programes />
